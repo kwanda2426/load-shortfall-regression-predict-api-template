8fa320292f664669c282c925e34ea03b38a92df2
Kwanda Mazibuko <90265907+kwanda2426@users.noreply.github.com>
2021-11-05 04:10:53 +0200

    Update model.py

--- model.py
+++ model.py
@@ -60,6 +60,23 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
+    feature_vector_df = feature_vector_df[['Unnamed: 0', 'time', 'Madrid_wind_speed', 'Valencia_wind_deg',
+       'Bilbao_rain_1h', 'Valencia_wind_speed', 'Seville_humidity',
+       'Madrid_humidity', 'Bilbao_clouds_all', 'Bilbao_wind_speed',
+       'Seville_clouds_all', 'Bilbao_wind_deg', 'Barcelona_wind_speed',
+       'Barcelona_wind_deg', 'Madrid_clouds_all', 'Seville_wind_speed',
+       'Barcelona_rain_1h', 'Seville_pressure', 'Seville_rain_1h',
+       'Bilbao_snow_3h', 'Barcelona_pressure', 'Seville_rain_3h',
+       'Madrid_rain_1h', 'Barcelona_rain_3h', 'Valencia_snow_3h',
+       'Madrid_weather_id', 'Barcelona_weather_id', 'Bilbao_pressure',
+       'Seville_weather_id', 'Valencia_pressure', 'Seville_temp_max',
+       'Madrid_pressure', 'Valencia_temp_max', 'Valencia_temp',
+       'Bilbao_weather_id', 'Seville_temp', 'Valencia_humidity',
+       'Valencia_temp_min', 'Barcelona_temp_max', 'Madrid_temp_max',
+       'Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp',
+       'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min',
+       'Madrid_temp', 'Madrid_temp_min', 'load_shortfall_3h']]
+    
     df_1 = feature_vector_df.copy() #creating a copy of data
 
     df_1['Valencia_pressure'].fillna(df_1['Valencia_pressure'].mean(), inplace=True) #imputing the null with the mean
